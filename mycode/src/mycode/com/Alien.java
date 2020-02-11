55c9a47b96eb596d2886c23d0d320df2196d63dd
USER <USER@HP>
2020-02-11 15:26:51 +0530

    first commit uploading all files

---
+++ mycode/src/mycode/com/Alien.java
@@ -0,0 +1,10 @@
+package mycode.com;
+
+public class Alien {
+public static void main(String[] args) {
+		/*
+		 * Calculator c=new Calculator(); System.out.println(c.add(10, 20));
+		 * System.out.println(c.sub(20, 10));
+		 */
+}
+}
